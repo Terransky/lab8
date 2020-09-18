6fa9072e016a344f04ffa902cdd3160a50015187
Anthony Turinski <nodeathking@gmail.com>
2020-09-17 18:31:10 -0700

    Finally got the JS to output

--- lab8.js
+++ lab8.js
@@ -1,29 +1,37 @@
 
-'use scrict'
+
 
 var num = 38;
-var guess = prompt("Please guess a number between 1 and 50 within 5 attempts.");
+var guess;
 
 
 
 
 for(var i = 0; i < 5; i++) {
 
-    if (guess<1 || guess>50) {
-        alert("Please enter a number between 1 and 50. You have " 5 - i "attempts remaining.");
-        i--;
+    guess = prompt("Please guess a number between 1 and 50 within 5 attempts.");
+    var remain = 4-i;
+    console.log(i);
+    console.log(guess);
+
+    if (guess==num) {
+        alert("Winner winner chicken dinner!")
+        break;
     }
 
-    else if (guess < num) {
-        alert("Guessed too low. You have " 5 - i "attempts remaining.");
+    else if (guess < num && guess >= 1) {
+        alert("Guessed too low. You have " + remain + " attempts remaining.");
     }
 
-    else if (guess > num) {
-        alert("Guessed too high. You have " 5 - i "attempts remaining.");
+    else if (guess > num && guess <= 50) {
+        alert("Guessed too high. You have " + remain + " attempts remaining.");
     }
 
     else {
-        alert("Winner winner chicken dinner!")
+        i--;
+        var plus1 = remain + 1;
+        alert("Please enter a number between 1 and 50. You have " + plus1 + " attempts remaining.");
+                
     }
 }
 
